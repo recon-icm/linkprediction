c6d580f1f0b7f95ce477d9665ecf9914b9dd161b
bchrol <bartek.chrol@gmail.com>
2014-11-04 15:25:50 +0100

    Added leicht-holme-newman global index

---
+++ R/similarity_lhn_global.R
@@ -0,0 +1,29 @@
+#' Leicht-Holme-Newman Global Index
+#'
+#' Recursive similarity measure based on all paths in a graph
+#'
+#' This function calculates vertex similarity based on the concept, that two
+#'   nodes are similar, when their neighbors are similar. Smaller \theta assings
+#'   more weight on shorter paths (closer neighbors are more important).
+#'
+#' @template sim
+#' @param \theta damping factor
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_lhn_global(g, 0.5)
+#'
+#' @export
+
+similarity_lhn_global <- function(graph, theta = 0.5){
+  lambda <- igraph::graph.eigen(graph)$value
+  deg <- igraph::degree(graph)
+  A <- igraph::get.adjacency(graph)
+  #I <- Diagonal(nrow(adj))
+  I <- diag(nrow(A))
+  tmp <- I - (theta / lambda) * A
+  score <- solve(tmp)
+  tmp <- outer(deg, deg)
+  score <- score / tmp
+  score
+}
