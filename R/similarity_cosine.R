8f2ef5ba6504d463e702d204f42ed881ee3460e2
bchrol <bartek.chrol@gmail.com>
2014-11-04 12:34:13 +0100

    Added cosine similarity

---
+++ R/similarity_cosine.R
@@ -0,0 +1,22 @@
+#' Cosine vertex similarity/ Salton index
+#'
+#' Similarity measure based on resource allocation process.
+#'
+#' This function measures the cosine of the angle between columns of
+#'   the adjacency matrix, corresponding to given nodes.
+#'
+#' @template sim
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_cosine(g)
+#'
+#' @export
+
+similarity_cosine <- function(graph){
+  deg <- igraph::degree(graph)
+
+  score <- igraph::cocitation(graph)
+  score <- score / outer(deg, deg, function(x, y) sqrt(x * y))
+  score
+}
