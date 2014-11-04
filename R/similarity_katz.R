c78ab40e254273943f70bb9986bd128f7b822595
bchrol <bartek.chrol@gmail.com>
2014-11-04 14:59:43 +0100

    Added katz index

---
+++ R/similarity_katz.R
@@ -0,0 +1,25 @@
+#' Katz Index
+#'
+#' Similarity measure based on all patsh in a graph
+#'
+#' This function counts all the paths between given pair of nodes, with shorter
+#'   paths counting more heavily. Weigths are exponential.
+#'
+#' @template sim
+#' @param \beta damping factor
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_katz(g, 0.1)
+#'
+#' @export
+
+similarity_katz <- function(graph, beta = 0.001){
+  A  <- igraph::get.adjacency(graph)
+  #I <- Diagonal(nrow(A))
+  I <- diag(nrow(A))
+  tmp <- I - beta * A
+  score <- solve(tmp)
+  diag(score) <- 0
+  score
+}
