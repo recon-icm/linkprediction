6e007eeba3c8cf2e78e6c5e451ffcec0411c03a5
bchrol <bartek.chrol@gmail.com>
2014-11-07 16:20:32 +0100

    Added pseudoinverse of laplacian similarity

---
+++ R/similarity_l.R
@@ -0,0 +1,23 @@
+#' Pseudoinverse of the Laplacian
+#'
+#' Similarity measure based solely on the pseudoinverse of the Laplacian matrix
+#'
+#' The pseudo inverse of the Laplacian matrix provides provides a direct measure
+#'   of similarity, as its elements are the inner products of vectors from an
+#'   Euclidean space, which preserves Average Commute Time between nodes.
+#'
+#' @template sim
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_l(g)
+#'
+#' @export
+
+similarity_l <- function(graph){
+  L <- igraph::graph.laplacian(graph)
+  n <- igraph::vcount(graph)
+
+  score <- solve(L - 1/n) + 1/n
+  score
+}
