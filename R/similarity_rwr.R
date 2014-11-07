b497734086ca537c34ae1f78b3a12a65edfdb035
bchrol <bartek.chrol@gmail.com>
2014-11-07 16:11:10 +0100

    Added random walk with restart

---
+++ R/similarity_rwr.R
@@ -0,0 +1,25 @@
+#' Random Walk with Restart
+#'
+#' Similarity measure based on random walker returning to its root
+#'
+#' It is an adaptation of PageRank algorithm. This function calculates
+#'   similarity between two nodes \eqn{a} and \eqn{b} as a sum of stationary
+#'   distributions of one node in random walk process starting from the other
+#'   node. Random walker could return to its root at every step with fixed
+#'   fixed probability \eqn{\alpha}.
+#'
+#' @template sim
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_rwr(g, alpha = 0.3)
+#'
+#' @export
+
+similarity_rwr <- function(graph, alpha = 0.3){
+  P <- igraph::get.stochastic(graph)
+  score <- solve(diag(nrow(P)) - (1 - alpha) * t(P))
+  score <- score * alpha
+  score <- score + t(score)
+  score
+}
