3c473a462ae85125d69b7b7be1e3d7da19625f6c
bchrol <bartek.chrol@gmail.com>
2014-11-13 14:36:27 +0100

    Added local path index

---
+++ R/similarity_local_path.R
@@ -0,0 +1,22 @@
+#' Local Paths Index
+#'
+#' Similarity measure based on the neighborhood of order 2
+#'
+#' This function counts the number of two-paths and three-paths between nodes,
+#'   with three-paths weighted by a parameter \eqn{\epsilon}.
+#'
+#' @template sim
+#' @param eps the importance of paths of length three
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_local_path(g, eps = 0.01)
+#'
+#' @export
+
+similarity_local_path <- function(graph, eps = 0.01){
+  A <- igraph::get.adjacency(graph)
+  score <- A %*% A
+  score <- score + score %*% A * eps
+  score
+}
