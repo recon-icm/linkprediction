5c5ac935eded5b40998f62da05d4f7a2835621a2
bchrol <bartek.chrol@gmail.com>
2014-12-03 12:57:49 +0100

    Adding matrix forest index

---
+++ R/similarity_matrix_forest.R
@@ -0,0 +1,23 @@
+#' Matrix Forest Index
+#'
+#' Similarity measure based on the concept of spanning trees
+#'
+#' This measure can be understood as the ratio of the number of spanning rooted
+#'   forest such that nodes \eqn{x} and \eqn{y} belong to the same tree rooted
+#'   at \eqn{x} to all spanning rooted forests of the network.
+#'
+#' @template sim
+#'
+#' @examples
+#' g <- igraph::random.graph.game(20, 0.3)
+#' similarity_matrix_forest(g)
+#'
+#' @export
+
+similarity_matrix_forest <- function(graph){
+  L <- igraph::graph.laplacian(graph)
+  n <- igraph::vcount(graph)
+
+  score <- solve(diag(n) + L)
+  score
+}
