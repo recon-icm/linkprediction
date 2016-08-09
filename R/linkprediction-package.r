7ec1df4be08f4eb3d95ea034cba0d5a815ab858d
Michal Bojanowski <michal2992@gmail.com>
2016-08-09 13:51:26 +0200

    Roxygenizations

--- R/linkprediction-package.r
+++ R/linkprediction-package.r
@@ -4,10 +4,10 @@
 #' prediction in graphs. See \code{\link{proxfun}}.
 #' 
 #' @note 
-#' Authors thanks (Polish) National Science Centre for support through SONATA 
-#' grant for the project \em{Dynamics of Competition and Collaboration  in
-#' Science: Individual Strategies, Collaboration Networks, and Organizational
-#' Hierarchies} (\url{recon.icm.edu.pl}).
+#' Authors thank (Polish) National Science Centre for support through SONATA 
+#' grant 2012/07/D/HS6/01971 for the project \emph{Dynamics of Competition and
+#' Collaboration  in Science: Individual Strategies, Collaboration Networks, and
+#' Organizational Hierarchies} (\url{recon.icm.edu.pl}).
 #'
 #' @name linkprediction
 #' @docType package
