a48371751ee4789c41f3f1d47ac19a2ca4c8337d
Michal Bojanowski <michal2992@gmail.com>
2016-08-09 13:50:20 +0200

    Added NCN info

--- R/linkprediction-package.r
+++ R/linkprediction-package.r
@@ -1,4 +1,13 @@
-#' linkprediction.
+#' Link Prediction Methods
+#' 
+#' Implements most of existing methods proximity-based methods of link
+#' prediction in graphs. See \code{\link{proxfun}}.
+#' 
+#' @note 
+#' Authors thanks (Polish) National Science Centre for support through SONATA 
+#' grant for the project \em{Dynamics of Competition and Collaboration  in
+#' Science: Individual Strategies, Collaboration Networks, and Organizational
+#' Hierarchies} (\url{recon.icm.edu.pl}).
 #'
 #' @name linkprediction
 #' @docType package
