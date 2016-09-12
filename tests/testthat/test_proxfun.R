e2d829635c0859dd82f76454c7232abe23e31277
Michal Bojanowski <michal2992@gmail.com>
2016-09-12 10:18:59 +0200

    Added rudimentary proxfun tests

--- tests/testthat/test_proxfun.R
+++ tests/testthat/test_proxfun.R
@@ -11,3 +11,20 @@ test_that("Wrong vertices sequences throw an error",{
   expect_error(proxfun(g, "cn", v1 = 1, v2 = 6))
   expect_error(proxfun(g, "cn", v1 = 1, v2 = "f"))
 })
+
+
+test_that("Error is thrown for empty network", {
+  expect_error( proxfun( igraph::make_empty_graph(n=4, directed=FALSE) ) )
+  expect_error( proxfun( igraph::make_empty_graph(n=4, directed=TRUE) ) )
+})
+
+
+test_that("Error is thrown for disconnected networks", {
+  expect_error( proxfun(igraph::make_graph(c(1,2, 3,4), directed=FALSE)) )
+})
+
+test_that("Error is thrown for directed networks", {
+  expect_error(
+    proxfun( igraph::make_graph(c(1,2, 2,3, 3,4), directed=TRUE) )
+  )
+})
