3f374cb3fc818449c70fa003917a71428f1c9f19
Michal Bojanowski <michal2992@gmail.com>
2017-09-20 15:55:28 +0200

    Static citations in proxfun

--- R/proxfun.R
+++ R/proxfun.R
@@ -12,58 +12,55 @@
 #' @param ... additional arguments specific for a selected measure
 #'
 #' @details 
-#' \Sexpr[stage=build,results=verbatim]{{
-#' keys <- c("adamic", "fouss", "pagerank", "zhou2009", "sorensen", "salton", "ravasz", "barabasi1999", "jaccard", "katz", "leicht", "mfi")
-#' bibdb <- bibtex::read.bib("vignettes/refs.bib")
-#' bibdb <- bibdb[keys]
-#' } }
-#' 
-#' This function calculates vertex proximity with the
-#' selected method and, optionally, between vertices
-#' specified with \code{v1} and \code{v2}. The following \code{method}s
-#' are available:
+#' This function calculates vertex proximity with the 
+#' selected method and, optionally, between vertices 
+#' specified with \code{v1} and \code{v2}. The following
+#' \code{method}s are available:
 #'
 #' \describe{
-#'  \item{\code{aa}}{Adamic-Adar index \Sexpr[stage=build]{cite("adamic", bibdb)}}
-#'  \item{\code{act}}{Average Commute Time \Sexpr[stage=build]{cite("fouss", bibdb)}}
-#'  \item{\code{act_n}}{Normalized Average Commute Time \Sexpr[stage=build]{cite("fouss", bibdb)}}
+#'  \item{\code{aa}}{Adamic-Adar index  (Adamic and Adar 2001)}
+#'  \item{\code{act}}{Average Commute Time (Fouss, Pirotte, Renders, and Saerens 2007)}
+#'  \item{\code{act_n}}{Normalized Average Commute Time (Fouss et al. 2007)}
 #'  \item{\code{cn}}{Common Neighbours}
-#'  \item{\code{cos}}{Cosine similarity \Sexpr[stage=build]{cite("salton", bibdb)}}
-#'  \item{\code{cos_l}}{cosine similarity on L+ \Sexpr[stage=build]{cite("fouss", bibdb)}}
+#'  \item{\code{cos}}{Cosine similarity (Salton and McGill 1986)}
+#'  \item{\code{cos_l}}{cosine similarity on L+ (Fouss et al. 2007)}
 #'  \item{\code{dist}}{graph distance}
-#'  \item{\code{hdi}}{Hub Depressed Index \Sexpr[stage=build]{cite("ravasz", bibdb)}}
-#'  \item{\code{hpi}}{Hub Promoted Index \Sexpr[stage=build]{cite("ravasz", bibdb)}}
-#'  \item{\code{jaccard}}{Jaccard coefficient \Sexpr[stage=build]{cite("jaccard", bibdb)} \Sexpr[stage=build]{cite("adamic", bibdb)}}
-#'  \item{\code{katz}}{Katz index \Sexpr[stage=build]{cite("katz", bibdb)}}
-#'  \item{\code{l}}{L+ directly \Sexpr[stage=build]{cite("fouss", bibdb)}}
-#'  \item{\code{lhn_local}}{Leicht-Holme-Newman Index \Sexpr[stage=build]{cite("leicht", bibdb)}}
-#'  \item{\code{lhn_global}}{Leicht-Holme-Newman Index global version \Sexpr[stage=build]{cite("leicht", bibdb)}}
-#'  \item{\code{lp}}{Local Path Index \Sexpr[stage=build]{cite("zhou2009", bibdb)}}
-#'  \item{\code{mf}}{Matrix Forest Index \Sexpr[stage=build]{cite("mfi", bibdb)}}
-#'  \item{\code{pa}}{preferential attachment \Sexpr[stage=build]{cite("barabasi1999", bibdb)}}
-#'  \item{\code{ra}}{resource allocation \Sexpr[stage=build]{cite("zhou2009", bibdb)}}
-#'  \item{\code{rwr}}{random walk with restart \Sexpr[stage=build]{cite("pagerank", bibdb)}}
-#'  \item{\code{sor}}{sorensen index/ dice coefficient \Sexpr[stage=build]{cite("sorensen", bibdb)}}
+#'  \item{\code{hdi}}{Hub Depressed Index (Ravasz, Somera, Mongru, Oltvai, and Barabási 2002)}
+#'  \item{\code{hpi}}{Hub Promoted Index (Ravasz et al. 2002)}
+#'  \item{\code{jaccard}}{Jaccard coefficient (Jaccard 1912)}
+#'  \item{\code{katz}}{Katz index (Katz 1953)}
+#'  \item{\code{l}}{L+ directly (Fouss et al. 2007)}
+#'  \item{\code{lhn_local}}{Leicht-Holme-Newman Index (Leicht, Holme, and Newman 2006)}
+#'  \item{\code{lhn_global}}{Leicht-Holme-Newman Index global version (Leicht et al. 2006)}
+#'  \item{\code{lp}}{Local Path Index (Zhou, Lü, and Zhang 2009)}
+#'  \item{\code{mf}}{Matrix Forest Index (Chebotarev P. Yu. 1997)}
+#'  \item{\code{pa}}{preferential attachment (Barabási and Albert 1999)}
+#'  \item{\code{ra}}{resource allocation (Zhou et al. 2009)}
+#'  \item{\code{rwr}}{random walk with restart (Brin and Page 1998)}
+#'  \item{\code{sor}}{sorensen index/dice coefficient (Sørensen 1948)}
 #' }
 #'
 #'
 #'
 #' @return 
-#' If \code{value = "matrix"} a matrix with \code{length(v1)} rows and
-#'   \code{length(v2)} with \code{rownames} and \code{colnames} equal to
-#'   \code{v1} and \code{v2} respectively.
-#'   If \code{value = "edgelist"} a \code{data.frame} with three columns:
-#'   \describe{
-#'     \item{from}{ID of a start node of an edge}
-#'     \item{to}{ID of an end node of an edge}
-#'     \item{value}{similarity score for that edge}
-#'     }
-#'   Edges with similarity score 0 are omitted.
-#'   If \code{value = "graph"} an object of class \code{igraph} or \code{network},
-#'   depending on the class of input graph. Returned graph has the same structure
-#'   (graph and node attributes, etc.) as the input graph, except for edges -
-#'   original edges are skipped, and new edges with positive similarity score
-#'   are added. Edged attribute "weight" indicates similarity score.
+#' If \code{value = "matrix"} a matrix with \code{length(v1)}
+#' rows and \code{length(v2)} with \code{rownames} and
+#' \code{colnames} equal to \code{v1} and \code{v2}
+#' respectively. If \code{value = "edgelist"} a
+#' \code{data.frame} with three columns:
+#' \describe{
+#'  \item{from}{ID of a start node of an edge}
+#'  \item{to}{ID of an end node of an edge}
+#'  \item{value}{similarity score for that edge}
+#' }
+#' Edges with similarity score 0 are omitted. If \code{value 
+#' = "graph"} an object of class \code{igraph} or 
+#' \code{network}, depending on the class of input graph. 
+#' Returned graph has the same structure (graph and node 
+#' attributes, etc.) as the input graph, except for edges - 
+#' original edges are skipped, and new edges with positive 
+#' similarity score are added. Edged attribute "weight" 
+#' indicates similarity score.
 #'   
 #' @references 
 #' Adamic L and Adar E (2001). “Friends and Neighbors on the Web.” _Social Networks_, *25*,
@@ -83,7 +80,7 @@
 #' Similarities Between Nodes of a Graph with Application to Collaborative Recommendation.”
 #' _IEEE Transactions on Knowledge and Data Engineering_, *19*(3), pp. 355-369.
 #' 
-#' Jaccard P (1912). “The DISTRIBUTION OF THE FLORA IN THE ALPINE ZONE.1.” _New
+#' Jaccard P (1912). “The Distribution of the Flora in the Alpine Zone 1” _New
 #' Phytologist_, *11*(2), pp. 37-50.
 #' 
 #' Katz L (1953). “A new status index derived from sociometric analysis.” _Psychometrika_,
@@ -96,7 +93,7 @@
 #' Organization of Modularity in Metabolic Networks.” _Science_, *297*(5586), pp. 1551-1555.
 #' 
 #' Salton G and McGill MJ (1986). _Introduction to Modern Information Retrieval_.
-#' McGraw-Hill, Inc., New York, NY, USA. ISBN 0070544840.
+#' McGraw-Hill, Inc., New York, NY, USA.
 #' 
 #' Sørensen T (1948). “A Method of Establishing Groups of Equal Amplitude in Plant Sociology
 #' Based on Similarity of Species Content and Its Application to Analyses of the Vegetation
