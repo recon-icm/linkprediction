3c163ea3d9d317ae1044a431cc5705de918f080d
Michal Bojanowski <michal2992@gmail.com>
2017-12-20 14:14:44 +0100

    Added some examples
    Closes #21

--- R/proxfun.R
+++ R/proxfun.R
@@ -106,6 +106,17 @@
 #'
 #' @export
 #' @encoding UTF-8
+#' 
+#' @examples 
+#' if(requireNamespace("igraph")) {
+#'   g <- igraph::make_graph(~ A -- C:D:E -- B -- F -- G:H -- I)
+#'   
+#' # Adamic-Adar
+#' proxfun(g, method="aa", value="edgelist")
+#'   
+#' # Random Walk with Restart
+#' proxfun(g, method="rwr", value="edgelist")
+#' }
 
 proxfun <- function(graph, ...){
   UseMethod("proxfun")
